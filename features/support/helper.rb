3b48ee98caef30a6999d87d6cc3b77788aa42d37
LumenRuby <88425511+LumenRuby@users.noreply.github.com>
2023-12-09 18:42:58 -0300

    Rmagick gem added

--- features/support/helper.rb
+++ features/support/helper.rb
@@ -1,5 +1,6 @@
 require 'capybara/cucumber'
 require 'rspec'
+require 'rmagick'
 
 Capybara.default_driver = :selenium_chrome # You can change this based on your preferred driver
 Capybara.default_max_wait_time = 10
