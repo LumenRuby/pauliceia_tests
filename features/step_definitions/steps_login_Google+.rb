b9b320ab1c9afa31e51d34d8a95144e39c0dc449
Bruno Alves Catão Silva <82419968+bcatao92@users.noreply.github.com>
2023-12-10 14:20:32 -0300

    Update steps_login_Google+.rb

--- features/step_definitions/steps_login_Google+.rb
+++ features/step_definitions/steps_login_Google+.rb
@@ -1,5 +1,5 @@
 require 'selenium-webdriver'
 
 Then('I should click the {string} button and be sent to an accounts.google.com/signin/oauth page') do |google_button_text|
-  click_link button_text # Implement code to click the specified Google+ button and verify the redirection
+  click_link button_text 
 end
